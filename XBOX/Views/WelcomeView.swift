b04b9b1d485a9485b9dbfecd3efcc463a27c7f51
Anagh Sharma <anaghsharma12@gmail.com>
2020-07-20 15:59:02 +0530

    Added drag gesture on welcome screen

--- XBOX/Views/WelcomeView.swift
+++ XBOX/Views/WelcomeView.swift
@@ -9,7 +9,10 @@
 import SwiftUI
 
 struct WelcomeView: View {
-    @State private var showOnboarding = false
+    @State private var showOnboarding = true
+    @State private var offsetController = CGSize.zero
+    @State private var offsetPerson = CGSize.zero
+    @State private var offsetBag = CGSize.zero
  
     var body: some View {
         ZStack {
@@ -37,22 +40,78 @@ struct WelcomeView: View {
                     .padding(.bottom, 56)
                 
                 HStack(alignment: .center, spacing: 16) {
-                    Image(systemName: "gamecontroller")
-                        .font(.system(size: 24, weight: .bold))
-                        .frame(width: 64, height: 64)
-                        .background(VisualEffectView(material: .systemUltraThinMaterial))
-                        .clipShape(Circle())
-                    Image(systemName: "person.crop.circle")
-                        .font(.system(size: 24, weight: .bold))
-                        .frame(width: 64, height: 64)
-                        .background(VisualEffectView(material: .systemUltraThinMaterial))
-                        .clipShape(Circle())
-                    Image(systemName: "bag")
-                        .font(.system(size: 24, weight: .bold))
-                        .frame(width: 64, height: 64)
-                        .padding(.bottom, 4)
-                        .background(VisualEffectView(material: .systemUltraThinMaterial))
-                        .clipShape(Circle())
+                    ZStack {
+                        Circle()
+                            .fill(Color.black)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "gamecontroller")
+                            .font(.system(size: 24, weight: .bold))
+                            .frame(width: 64, height: 64)
+                            .background(VisualEffectView(material: .systemUltraThinMaterial))
+                            .clipShape(Circle())
+                            .offset(y: offsetController.height)
+                            .animation(.easeOut(duration: 0.3))
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { gesture in
+                                        if gesture.translation.height > 0 && gesture.translation.height <= 100 {
+                                            self.offsetController = gesture.translation
+                                        }
+                                }
+                                .onEnded { _ in
+                                    self.offsetController = CGSize.zero
+                                }
+                        )
+                    }
+                        
+                    ZStack {
+                        Circle()
+                            .fill(Color.black)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "person.crop.circle")
+                            .font(.system(size: 24, weight: .bold))
+                            .frame(width: 64, height: 64)
+                            .background(VisualEffectView(material: .systemUltraThinMaterial))
+                            .clipShape(Circle())
+                            .offset(y: offsetPerson.height)
+                            .animation(.easeOut(duration: 0.3))
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { gesture in
+                                        if gesture.translation.height > 0 && gesture.translation.height <= 100 {
+                                            self.offsetPerson = gesture.translation
+                                        }
+                                }
+                                .onEnded { _ in
+                                    self.offsetPerson = CGSize.zero
+                                }
+                        )
+                    }
+                    
+                    ZStack {
+                        Circle()
+                            .fill(Color.black)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "bag")
+                            .font(.system(size: 24, weight: .bold))
+                            .frame(width: 64, height: 64)
+                            .padding(.bottom, 4)
+                            .background(VisualEffectView(material: .systemUltraThinMaterial))
+                            .clipShape(Circle())
+                            .offset(y: offsetBag.height)
+                                .animation(.easeOut(duration: 0.3))
+                                .gesture(
+                                    DragGesture()
+                                        .onChanged { gesture in
+                                            if gesture.translation.height > 0 && gesture.translation.height <= 100 {
+                                                self.offsetBag = gesture.translation
+                                            }
+                                    }
+                                    .onEnded { _ in
+                                        self.offsetBag = CGSize.zero
+                                    }
+                        )
+                    }
                 }
                 .padding(.bottom, 128)
                 
